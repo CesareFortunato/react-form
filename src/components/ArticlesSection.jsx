0383e949467bc6e2f466e0a5aa068eea82fe3616
CesareFortunato <cesare.fortunato89@gmail.com>
2026-01-22 15:07:39 +0100

    feat: setup form input

--- src/components/ArticlesSection.jsx
+++ src/components/ArticlesSection.jsx
@@ -4,13 +4,25 @@ function ArticlesSection() {
 
     const initialArticles = ["titolo1", "titolo2", "titolo3"];
 
-    const [articles, setArticles] = useState(initialArticles)
+    const [articles, setArticles] = useState(initialArticles);
+    const [newArticle, setNewArticle] = useState('Nuovo Articolo')
+
+    const handleSubmit = () => {
+
+    }
 
     return (
+        <>
         <ul>
             {articles.map((article, index) => ( <li key={index}>{article}</li>))}
         </ul>
-
+        <form onSubmit={handleSubmit}>
+        <input type="text"
+        value={newArticle}
+        onChange={e => {setNewArticle(e.target.value)}}
+        />
+        </form>
+        </>
     )
 }
 
